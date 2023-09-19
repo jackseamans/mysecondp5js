186be92fb95f42f89a382aa6f192073c093afff9
JACK SEAMANS <JJS22F@FSU.EDU>
2023-09-19 17:15:20 -0400

    Number 1 and 2

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+let x = 200;
+let speed = 2;
+let diameter = 40;
+
 function setup() {
-  createCanvas(400, 400);
+
+  createCanvas(400, 400);  
 }
 
-function draw() {
+function draw (){
   background(125);
+  ellipse(x, height/2, 40);
+
+  if (x >= width || x <= 0) {
+  speed = speed * -1
+  }
+
+  x += speed;
 }
+
+
+function mousePressed() {
+   noLoop();
+
+ }
+   function mouseReleased() {
+     loop();
+  
+   }
